881abcd6eec261a229e48e67b319339d367799fe
jonathan <jonathan.willian321@gmail.com>
2024-12-22 18:11:09 -0300

    added breath first has a path finding algorithm to ghosts

--- pacman/main.c
+++ pacman/main.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
+#include <stdlib.h>
 
 #include "raylib.h"
 #include "raymath.h"
 
 #define DEBUG
 
+#define ARRAY_SIZE(arr) (sizeof((arr))/sizeof((arr)[0]))
 #define BACKGROUD_COLOR BLACK
+
 #define SWIDTH  800
 #define SHEIGHT 800
-
-#define WORLD_OFFESET ((Vector2) { .x = 100, .y = 10 })
-
-#define VECTOR2(xx, yy) ((Vector2) { .x = (xx), .y = (yy) })
-
 #define WWIDTH  28
 #define WHEIGHT 31
+
 #define BLOCK_WIDTH  (SWIDTH / WWIDTH)
 #define BLOCK_HEIGHT (SHEIGHT / WHEIGHT)
 
+#define OUT_OF_BOUNDS(v) ( (v).x < 0 || (v).x >= WWIDTH ) || ( (v).y < 0 || (v).y >= WHEIGHT )
+
+#define WORLD_OFFESET ((Vector2) { .x = 100, .y = 10 })
+#define VECTOR2(xx, yy) ((Vector2) { .x = (xx), .y = (yy) })
+
 #define TIME_TO_NEXT_BLK 0.150
+
+#define CAPACITY 64
+#define DA_APPEND(da, v) { \
+    if ((da).items == NULL) { \
+        (da).items = malloc(sizeof((v)) * CAPACITY); \
+        (da).len = 0; \
+        (da).capacity = CAPACITY; \
+    } \
+    if ((da).capacity <= (da).len) { \
+        (da).items = realloc((da).items, sizeof((v)) * 2*(da).capacity); \
+        (da).capacity = 2*(da).capacity; \
+    } \
+    (da).items[(da).len++] = v; \
+} while (0);
 
 typedef enum {
     WALL_1 = '#',
@@ -61,6 +79,39 @@ Blocks world[WHEIGHT][WWIDTH] = {
     { '#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#' },
 };
 
+Vector2 validPositions[] = {
+    VECTOR2( 1,  1), VECTOR2( 2,  1), VECTOR2( 3,  1), VECTOR2( 4,  1), VECTOR2( 5,  1), VECTOR2( 6,  1), VECTOR2( 7,  1), VECTOR2( 8,  1), VECTOR2( 9,  1), VECTOR2(10,  1),
+    VECTOR2(11,  1), VECTOR2(12,  1), VECTOR2(15,  1), VECTOR2(16,  1), VECTOR2(17,  1), VECTOR2(18,  1), VECTOR2(19,  1), VECTOR2(20,  1), VECTOR2(21,  1), VECTOR2(22,  1),
+    VECTOR2(23,  1), VECTOR2(24,  1), VECTOR2(25,  1), VECTOR2(26,  1), VECTOR2( 1,  2), VECTOR2( 6,  2), VECTOR2(12,  2), VECTOR2(15,  2), VECTOR2(21,  2), VECTOR2(26,  2),
+    VECTOR2( 1,  3), VECTOR2( 6,  3), VECTOR2(12,  3), VECTOR2(15,  3), VECTOR2(21,  3), VECTOR2(26,  3), VECTOR2( 1,  4), VECTOR2( 6,  4), VECTOR2(12,  4), VECTOR2(15,  4),
+    VECTOR2(21,  4), VECTOR2(26,  4), VECTOR2( 1,  5), VECTOR2( 2,  5), VECTOR2( 3,  5), VECTOR2( 4,  5), VECTOR2( 5,  5), VECTOR2( 6,  5), VECTOR2( 7,  5), VECTOR2( 8,  5),
+    VECTOR2( 9,  5), VECTOR2(10,  5), VECTOR2(11,  5), VECTOR2(12,  5), VECTOR2(13,  5), VECTOR2(14,  5), VECTOR2(15,  5), VECTOR2(16,  5), VECTOR2(17,  5), VECTOR2(18,  5),
+    VECTOR2(19,  5), VECTOR2(20,  5), VECTOR2(21,  5), VECTOR2(22,  5), VECTOR2(23,  5), VECTOR2(24,  5), VECTOR2(25,  5), VECTOR2(26,  5), VECTOR2( 1,  6), VECTOR2( 6,  6),
+    VECTOR2( 9,  6), VECTOR2(18,  6), VECTOR2(21,  6), VECTOR2(26,  6), VECTOR2( 1,  7), VECTOR2( 6,  7), VECTOR2( 9,  7), VECTOR2(18,  7), VECTOR2(21,  7), VECTOR2(26,  7),
+    VECTOR2( 1,  8), VECTOR2( 2,  8), VECTOR2( 3,  8), VECTOR2( 4,  8), VECTOR2( 5,  8), VECTOR2( 6,  8), VECTOR2( 9,  8), VECTOR2(10,  8), VECTOR2(11,  8), VECTOR2(12,  8),
+    VECTOR2(15,  8), VECTOR2(16,  8), VECTOR2(17,  8), VECTOR2(18,  8), VECTOR2(21,  8), VECTOR2(22,  8), VECTOR2(23,  8), VECTOR2(24,  8), VECTOR2(25,  8), VECTOR2(26,  8),
+    VECTOR2( 6,  9), VECTOR2(12,  9), VECTOR2(15,  9), VECTOR2(21,  9), VECTOR2( 6, 10), VECTOR2(12, 10), VECTOR2(15, 10), VECTOR2(21, 10), VECTOR2( 6, 11), VECTOR2(12, 11),
+    VECTOR2(15, 11), VECTOR2(21, 11), VECTOR2( 6, 12), VECTOR2( 9, 12), VECTOR2(10, 12), VECTOR2(11, 12), VECTOR2(12, 12), VECTOR2(13, 12), VECTOR2(14, 12), VECTOR2(15, 12),
+    VECTOR2(16, 12), VECTOR2(17, 12), VECTOR2(18, 12), VECTOR2(21, 12), VECTOR2( 6, 13), VECTOR2( 9, 13), VECTOR2(13, 13), VECTOR2(14, 13), VECTOR2(18, 13), VECTOR2(21, 13),
+    VECTOR2( 6, 14), VECTOR2( 7, 14), VECTOR2( 8, 14), VECTOR2( 9, 14), VECTOR2(18, 14), VECTOR2(19, 14), VECTOR2(20, 14), VECTOR2(21, 14), VECTOR2(22, 14), VECTOR2(23, 14),
+    VECTOR2(24, 14), VECTOR2(25, 14), VECTOR2(26, 14), VECTOR2(27, 14), VECTOR2( 6, 15), VECTOR2( 9, 15), VECTOR2(18, 15), VECTOR2(21, 15), VECTOR2( 6, 16), VECTOR2( 9, 16),
+    VECTOR2(18, 16), VECTOR2(21, 16), VECTOR2( 6, 17), VECTOR2( 9, 17), VECTOR2(10, 17), VECTOR2(11, 17), VECTOR2(12, 17), VECTOR2(13, 17), VECTOR2(14, 17), VECTOR2(15, 17),
+    VECTOR2(16, 17), VECTOR2(17, 17), VECTOR2(18, 17), VECTOR2(21, 17), VECTOR2( 6, 18), VECTOR2( 9, 18), VECTOR2(18, 18), VECTOR2(21, 18), VECTOR2( 6, 19), VECTOR2( 9, 19),
+    VECTOR2(18, 19), VECTOR2(21, 19), VECTOR2( 1, 20), VECTOR2( 2, 20), VECTOR2( 3, 20), VECTOR2( 4, 20), VECTOR2( 5, 20), VECTOR2( 6, 20), VECTOR2( 7, 20), VECTOR2( 8, 20),
+    VECTOR2( 9, 20), VECTOR2(10, 20), VECTOR2(11, 20), VECTOR2(12, 20), VECTOR2(15, 20), VECTOR2(16, 20), VECTOR2(17, 20), VECTOR2(18, 20), VECTOR2(19, 20), VECTOR2(20, 20),
+    VECTOR2(21, 20), VECTOR2(22, 20), VECTOR2(23, 20), VECTOR2(24, 20), VECTOR2(25, 20), VECTOR2(26, 20), VECTOR2( 1, 21), VECTOR2( 6, 21), VECTOR2(12, 21), VECTOR2(15, 21),
+    VECTOR2(21, 21), VECTOR2(26, 21), VECTOR2( 1, 22), VECTOR2( 6, 22), VECTOR2(12, 22), VECTOR2(15, 22), VECTOR2(21, 22), VECTOR2(26, 22), VECTOR2( 1, 23), VECTOR2( 2, 23),
+    VECTOR2( 3, 23), VECTOR2( 6, 23), VECTOR2( 7, 23), VECTOR2( 8, 23), VECTOR2( 9, 23), VECTOR2(10, 23), VECTOR2(11, 23), VECTOR2(12, 23), VECTOR2(13, 23), VECTOR2(14, 23),
+    VECTOR2(15, 23), VECTOR2(16, 23), VECTOR2(17, 23), VECTOR2(18, 23), VECTOR2(19, 23), VECTOR2(20, 23), VECTOR2(21, 23), VECTOR2(24, 23), VECTOR2(25, 23), VECTOR2(26, 23),
+    VECTOR2( 3, 24), VECTOR2( 6, 24), VECTOR2( 9, 24), VECTOR2(18, 24), VECTOR2(21, 24), VECTOR2(24, 24), VECTOR2( 3, 25), VECTOR2( 6, 25), VECTOR2( 9, 25), VECTOR2(18, 25),
+    VECTOR2(21, 25), VECTOR2(24, 25), VECTOR2( 1, 26), VECTOR2( 2, 26), VECTOR2( 3, 26), VECTOR2( 4, 26), VECTOR2( 5, 26), VECTOR2( 6, 26), VECTOR2( 9, 26), VECTOR2(10, 26),
+    VECTOR2(11, 26), VECTOR2(12, 26), VECTOR2(15, 26), VECTOR2(16, 26), VECTOR2(17, 26), VECTOR2(18, 26), VECTOR2(21, 26), VECTOR2(22, 26), VECTOR2(23, 26), VECTOR2(24, 26),
+    VECTOR2(25, 26), VECTOR2(26, 26), VECTOR2( 1, 27), VECTOR2(12, 27), VECTOR2(15, 27), VECTOR2(26, 27), VECTOR2( 1, 28), VECTOR2(12, 28), VECTOR2(15, 28), VECTOR2(26, 28),
+    VECTOR2( 1, 29), VECTOR2( 2, 29), VECTOR2( 3, 29), VECTOR2( 4, 29), VECTOR2( 5, 29), VECTOR2( 6, 29), VECTOR2( 7, 29), VECTOR2( 8, 29), VECTOR2( 9, 29), VECTOR2(10, 29),
+    VECTOR2(11, 29), VECTOR2(12, 29), VECTOR2(13, 29), VECTOR2(14, 29), VECTOR2(15, 29), VECTOR2(16, 29), VECTOR2(17, 29), VECTOR2(18, 29), VECTOR2(19, 29), VECTOR2(20, 29),
+    VECTOR2(21, 29), VECTOR2(22, 29), VECTOR2(23, 29), VECTOR2(24, 29), VECTOR2(25, 29), VECTOR2(26, 29),
+};
+
 static bool quit = false;
 
 Vector2 world2screen(Vector2 w) {
@@ -93,6 +144,14 @@ struct Pacman {
 
 struct Ghost {
     Entity entity;
+    Vector2 target;
+    struct {
+        Direction *items;
+        size_t len;
+        size_t capacity;
+        size_t curr;
+    } path;
+
 };
 
 static bool pacman_moving = false;
@@ -112,32 +171,49 @@ static struct Ghost ghosts[] = {
         .entity = {
             .direction = RIGHT,
             .size = VECTOR2(BLOCK_WIDTH / 2, BLOCK_WIDTH / 2),
-            .pos = VECTOR2(1, 14),
+            .pos = VECTOR2(16, 14),
             .last_pos = VECTOR2(16, 14),
-            .screen_pos = { 0 }
-        }
+            .screen_pos = { 0 },
+        },
+        .target = VECTOR2(16, 14),
+        .path = { 0 }
     },
-    {
-        .entity = {
-            .direction = LEFT,
-            .size = VECTOR2(BLOCK_WIDTH / 2, BLOCK_WIDTH / 2),
-            .pos = VECTOR2(1, 14),
-            .last_pos = VECTOR2(16, 14),
-            .screen_pos = { 0 }
-        }
-    },
+    // {
+    //     .entity = {
+    //         .direction = LEFT,
+    //         .size = VECTOR2(BLOCK_WIDTH / 2, BLOCK_WIDTH / 2),
+    //         .pos = VECTOR2(15, 14),
+    //         .last_pos = VECTOR2(15, 14),
+    //         .screen_pos = { 0 }
+    //     },
+    //     .target = VECTOR2(15, 14),
+    //     .path = { 0 }
+    // },
+    // {
+    //     .entity = {
+    //         .direction = LEFT,
+    //         .size = VECTOR2(BLOCK_WIDTH / 2, BLOCK_WIDTH / 2),
+    //         .pos = VECTOR2(14, 14),
+    //         .last_pos = VECTOR2(14, 14),
+    //         .screen_pos = { 0 }
+    //     },
+    //     .target = VECTOR2(14, 14),
+    //     .path = { 0 }
+    // },
 };
 
 static_assert(sizeof(ghosts) > 0);
 const size_t TOTAL_GHOSTS = sizeof(ghosts)/sizeof(ghosts[0]);
 
+bool follow_pacman = false;
 void handle_input() {
     int k = GetKeyPressed();
     switch (k) {
-        case KEY_UP:    pacman.nextDirection = UP;    break;
-        case KEY_DOWN:  pacman.nextDirection = DOWN;  break;
-        case KEY_LEFT:  pacman.nextDirection = LEFT;  break;
-        case KEY_RIGHT: pacman.nextDirection = RIGHT; break;
+        case KEY_UP:    pacman.nextDirection = UP;     break;
+        case KEY_DOWN:  pacman.nextDirection = DOWN;   break;
+        case KEY_LEFT:  pacman.nextDirection = LEFT;   break;
+        case KEY_RIGHT: pacman.nextDirection = RIGHT;  break;
+        case KEY_X:     follow_pacman = !follow_pacman; break;
     }
 }
 
@@ -167,6 +243,10 @@ bool isTimeToMove(float dt) {
     return itsTime;
 }
 
+Vector2 calcNewPosition(Vector2 old_position, Direction d) {
+    return Vector2Add(old_position, directionVector(d));
+}
+
 void movePacman(void) {
     pacman_moving = false;
 
@@ -180,7 +260,7 @@ void movePacman(void) {
         }
     }
 
-    if (!moved) newpos = Vector2Add(pacman.entity.pos, directionVector(pacman.entity.direction));
+    if (!moved) newpos = calcNewPosition(pacman.entity.pos, pacman.entity.direction);
 
     if (!isWall(newpos)) {
         pacman.entity.last_pos = pacman.entity.pos;
@@ -190,18 +270,81 @@ void movePacman(void) {
     }
 }
 
+void tracePath(struct Ghost *ghost) {
+    if (Vector2Equals(ghost->target, ghost->entity.pos)) return;
+    static Direction all_directions[] = { UP, DOWN, LEFT, RIGHT };
+
+    struct { Direction direction; int px, py; }
+                squares[WHEIGHT][WWIDTH] = { 0 };
+    static Vector2 queue[WHEIGHT*WWIDTH] = { 0 };
+
+    bool visited[WHEIGHT][WWIDTH] = { 0 };
+
+    size_t qlen = 0;
+    size_t qhead = 0;
+    size_t l = ARRAY_SIZE(all_directions);
+    size_t limit = ARRAY_SIZE(queue);
+    queue[qlen++] = ghost->entity.pos;
+    visited[(int) ghost->entity.pos.y][(int) ghost->entity.pos.x] = true;
+    while (qlen <= limit && qhead < qlen) {
+        Vector2 current_position = queue[qhead++];
+
+        for (size_t d = 0; d < l; d++) {
+            Direction current_direction = all_directions[d];
+            Vector2 pos = calcNewPosition(current_position, current_direction);
+            int iy = pos.y;
+            int ix = pos.x;
+
+            if (OUT_OF_BOUNDS(pos) || visited[iy][ix] || isWall(pos)) continue;
+
+            squares[iy][ix].py = (int) current_position.y;
+            squares[iy][ix].px = (int) current_position.x;
+            squares[iy][ix].direction = current_direction;
+            visited[iy][ix] = true;
+            queue[qlen++] = pos;
+
+            if (Vector2Equals(ghost->target, pos)) goto found;
+        }
+    }
+
+    assert(0 && "how da fuck we didnt find the path?");
+found:
+    ghost->path.len = 0;
+    int ix = ghost->target.x;
+    int iy = ghost->target.y;
+
+    do {
+        assert(visited[iy][ix]);
+        DA_APPEND(ghost->path, squares[iy][ix].direction);
+        int temp = squares[iy][ix].px;
+        iy = squares[iy][ix].py;
+        ix = temp;
+    } while (!Vector2Equals(VECTOR2(ix, iy), ghost->entity.pos));
+
+    ghost->path.curr = ghost->path.len;
+}
+
+Direction ghostDirection(struct Ghost *ghost) {
+    return ghost->path.items[--ghost->path.curr];
+}
+
 void moveGhosts(void) {
     for (size_t i = 0; i < TOTAL_GHOSTS; i++) {
-        ghosts[i].entity.last_pos = ghosts[i].entity.pos;
-        ghosts[i].entity.screen_pos = world2screen(ghosts[i].entity.pos);
-        Vector2 new_pos = Vector2Add(ghosts[i].entity.pos, directionVector(ghosts[i].entity.direction));
-
-        for (Direction d = GetRandomValue(0, 3); isWall(new_pos); d++, d%=4) {
-            ghosts[i].entity.direction = d;
-            new_pos = Vector2Add(ghosts[i].entity.pos, directionVector(ghosts[i].entity.direction));
+        if (follow_pacman) {
+            ghosts[i].target = pacman.entity.pos;
+            tracePath(&ghosts[i]);
+        } else {
+            if (Vector2Equals(ghosts[i].entity.pos, ghosts[i].target)) {
+                do {
+                    ghosts[i].target = validPositions[GetRandomValue(0, ARRAY_SIZE(validPositions) - 1)];
+                } while (Vector2Equals(ghosts[i].entity.pos, ghosts[i].target));
+                tracePath(&ghosts[i]);
+            }
         }
 
-        ghosts[i].entity.pos = new_pos;
+        ghosts[i].entity.last_pos = ghosts[i].entity.pos;
+        ghosts[i].entity.direction = ghostDirection(&ghosts[i]);
+        ghosts[i].entity.pos = calcNewPosition(ghosts[i].entity.pos, ghosts[i].entity.direction);
     }
 }
 
@@ -295,8 +438,6 @@ void drawDebugInfo(void) {
 
     sprintf(s, "P(%.0f, %.0f)", pacman.entity.pos.x, pacman.entity.pos.y);
     DrawText(s, 10, 90, 10, WHITE);
-
-
 }
 
 void draw() {
@@ -311,15 +452,14 @@ void draw() {
 }
 
 int main(void) {
-    InitWindow(SWIDTH + WORLD_OFFESET.x*2, SHEIGHT + WORLD_OFFESET.y*2, "Pacman");
     pacman.entity.screen_pos = world2screen(pacman.entity.pos);
-
     for (size_t i = 0; i < TOTAL_GHOSTS; i++) {
         ghosts[i].entity.screen_pos = world2screen(ghosts[i].entity.pos);
     }
 
     SetRandomSeed(time(NULL));
 
+    InitWindow(SWIDTH + WORLD_OFFESET.x*2, SHEIGHT + WORLD_OFFESET.y*2, "Pacman");
     while (!WindowShouldClose() && !quit) {
         handle_input();
         update();
